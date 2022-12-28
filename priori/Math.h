598ae34ab4a72bb03007ab8807974657987f2c8d
Nathan <61805267+Nathan-925@users.noreply.github.com>
2022-12-27 22:51:57 -0500

    Added Math.h

---
+++ priori/Math.h
@@ -0,0 +1,13 @@
+/*
+ * Math.h
+ *
+ *  Created on: Dec 27, 2022
+ *      Author: Nathan
+ */
+
+#ifndef PRIORI_MATH_H_
+#define PRIORI_MATH_H_
+
+#include "Math/Interpolation.h"
+
+#endif /* PRIORI_MATH_H_ */
